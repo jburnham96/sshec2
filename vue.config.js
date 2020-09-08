36992868e55e5a87f39ff2de00015db9f8d379cd
James Burnham <labpd01@googlemail.com>
2020-09-08 19:08:41 +0100

    Removed webpack loader config

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,5 @@ module.exports = {
         nodeIntegration: true,
       },
     },
-    chainWebpack: config => {
-      config.module
-        .rule('node')
-        .test(/\.node$/)
-        .use('node-loader')
-        .loader('node-loader')
-        .end();
-    }
   };
   
